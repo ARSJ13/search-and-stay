4c29c6de3fc5412f8e21c2d0a1ea1d51588505d4
Avivaldo Ribeiro <avivaldo.ribeiro@gmail.com>
2022-12-22 14:59:37 -0300

    created pagination component

---
+++ components/Pagination.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-pagination
+    v-model="currentPage"
+    :total-rows="total"
+    :per-page="perPage"
+    first-number
+    last-number
+    @change="changePage"
+  ></b-pagination>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  data: () => {
+    return {
+      currentPage: 1  
+    }
+  },
+  props: {
+    total: {
+      type: Number,
+      require: true
+    },
+    perPage: {
+      type: Number,
+      require: true
+    }
+  }, 
+  methods: {
+    changePage(ev) {
+      this.$emit('UpdatePage', ev)
+    }
+  }
+}
+</script>
