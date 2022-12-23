637f9dc07fa91997f1a07d67273a0fc58937aa6a
Avivaldo Ribeiro <avivaldo.ribeiro@gmail.com>
2022-12-23 15:17:55 -0300

    implementation of CRUD in actions

--- store/index.js
+++ store/index.js
@@ -1,7 +1,12 @@
 export const state = () => ({
   token: '',
   list: [],
-  pagination: ''
+  pagination: '',
+  dataPattern: {
+    bg_color: '',
+    text_color: '',
+    active: null
+  }
 })
 
 export const getters = {
@@ -20,8 +25,14 @@ export const mutations = {
   SET_LIST (state, payload) {
     state.list = payload
   },
+  SET_LIST_CREATE (state, payload) {
+    state.list.push(payload)
+  },
   SET_PAGINATION (state, payload) {
     state.pagination = payload
+  },
+  SET_DATA_PATTERN (state, payload) {
+    state.dataPattern = payload
   }
 }
 
@@ -52,6 +63,56 @@ export const actions = {
         commit('SET_LIST', data.entities)
         commit('SET_PAGINATION', data.pagination)
         return data
+      })
+      .catch((err) => {
+        localStorage.removeItem('av_token')
+        return err
+      })
+  },
+  async create ({ state, commit }, data) {
+    const calendar_patterns = {
+      bg_color: data.bg,
+      text_color: data.text,
+      active: data.active
+    }
+    return await this.$axios
+      .$post(`/admin/calendar_patterns`, { calendar_patterns })
+      .then(({ data }) => {
+        commit('SET_LIST_CREATE', data)
+        return data
+      })
+      .catch((err) => {
+        localStorage.removeItem('av_token')
+        return err
+      })
+  },
+  async update ({ state, commit }, info) {
+    const calendar_patterns = {
+      bg_color: info.bg,
+      text_color: info.text,
+      active: info.active
+    }
+    return await this.$axios
+      .$put(`/admin/calendar_patterns/${info.id}`, { calendar_patterns })
+      .then(async ({ data }) => {
+        const updateList = await state.list.map((el) => {
+          if (data.id == el.id) {
+            return data
+          }
+          return el
+        });
+        commit('SET_LIST', updateList)
+      })
+      .catch((err) => {
+        localStorage.removeItem('av_token')
+        return err
+      })
+  },
+  async delete ({ dispatch }, id) {
+    return await this.$axios
+      .$delete(`/admin/calendar_patterns/${id}`)
+      .then(async () => {
+        await dispatch('listAll')
       })
       .catch((err) => {
         localStorage.removeItem('av_token')
